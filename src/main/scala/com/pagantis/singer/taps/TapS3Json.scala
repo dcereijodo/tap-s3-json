4d4a0da9e02bf8567b36b6b016106d7a293948b8
dcereijodo <dcereijo@digitalorigin.com>
2019-06-08 18:34:34 +0200

    graceful shutdown handling

--- src/main/scala/com/pagantis/singer/taps/TapS3Json.scala
+++ src/main/scala/com/pagantis/singer/taps/TapS3Json.scala
@@ -4,7 +4,6 @@ import akka.actor.ActorSystem
 import akka.event.{Logging, LoggingAdapter}
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
-import com.typesafe.config.ConfigFactory
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContextExecutor, Future}
@@ -25,7 +24,8 @@ object TapS3Json extends App {
   val startTime = System.nanoTime()
 
   // stream the bucket contents for the provided json paths as singer messages to stdout
-  S3Source.fromConfig.object_contents
+  val tapComputation =
+    S3Source.fromConfig.object_contents
     .log(clazz)
     .mapAsync(12)( contentLine => Future{ singerAdapter.toSingerRecord(contentLine) })
     .log(clazz)
@@ -33,29 +33,28 @@ object TapS3Json extends App {
     .mapAsync(12)(singerMessage => Future{ singerAdapter.toJsonString(singerMessage)})
     // TODO: instead of writing to stdout a cleaner approach would be using another logger for singer messages
     .runForeach(println(_))
-    // Now comes a fairly complicated shutdown sequence, which first shuts down the underlying http infrastructure
-    // and then terminates the materializer and actor system. This is to avoid akka to complaint about abrupt termination
-    // as it is described in this issue https://github.com/akka/akka-http/issues/497
-    // This shutdown sequence was copied from another related issue: https://github.com/akka/akka-http/issues/907#issuecomment-345288919
-    .onComplete(res => {
-        Http().shutdownAllConnectionPools.andThen { case _ =>
-          materializer.shutdown
-          system.terminate
-        } andThen {
-          Console.flush()
-          res match {
-            case Success(_) => {
-              standardLogger.info(s"Total execution time: ${(System.nanoTime() - startTime)/1000000000} seconds")
-              sys.exit(0)
-            }
-            case _ => sys.exit(1)
-          }
-        }
 
+  // Shutdown sequence is fairly. First shut down the underlying http infrastructure
+  // and then terminate the materializer and actor system. This is to avoid akka to complaint about abrupt termination
+  // as it is described in this issue https://github.com/akka/akka-http/issues/497
+  // This shutdown sequence was copied from another related issue: https://github.com/akka/akka-http/issues/907#issuecomment-345288919
+  def shutdownSequence = {
+    Http().shutdownAllConnectionPools flatMap ( _ => {
+        materializer.shutdown
+        system.terminate
       }
     )
+  }
 
-  // block main thread forever
-  // exit will be handled on the stream exit callback
-  Await.ready(Future.never, Duration.Inf)
+  Await.ready(tapComputation, Duration.Inf)
+
+  standardLogger.info(s"Total execution time: ${(System.nanoTime - startTime)/1000000000} seconds")
+
+  Await.ready(shutdownSequence, Duration.Inf)
+
+  tapComputation.value match {
+    case Some(Success(_)) => sys.exit(0)
+    case _ => sys.exit(1)
+  }
+
 }
