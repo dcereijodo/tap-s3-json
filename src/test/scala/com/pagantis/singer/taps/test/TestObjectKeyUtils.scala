b7ecb3a195ad9c289f9454e2444453d30b4bc9a2
dcereijodo <dcereijo@digitalorigin.com>
2019-07-02 17:16:36 +0200

    refactor: rename PartitioningUtils to ObjectKeyUtils

--- src/test/scala/com/pagantis/singer/taps/test/TestObjectKeyUtils.scala
+++ src/test/scala/com/pagantis/singer/taps/test/TestObjectKeyUtils.scala
@@ -1,10 +1,10 @@
 package com.pagantis.singer.taps.test
 
-import com.pagantis.singer.taps.PartitioningUtils
+import com.pagantis.singer.taps.ObjectKeyUtils
 import com.pagantis.singer.taps.exceptions.InvalidPrefixException
 import org.scalatest._
 
-class TestPartitioningUtils extends PartitioningUtils with Matchers with WordSpecLike {
+class TestObjectKeyUtils extends ObjectKeyUtils with Matchers with WordSpecLike {
   "PartitioningUtils" must {
     "build with S3 prefix" in {
       buildS3Prefix(Some("s3/prefix"), Some("date=2019-01-01")) shouldBe Some("s3/prefix/date=2019-01-01")
@@ -19,6 +19,23 @@ class TestPartitioningUtils extends PartitioningUtils with Matchers with WordSpe
       an [InvalidPrefixException] should be thrownBy {
         buildS3Prefix(Some(""), Some("date=2019-01-01"))
       }
+    }
+    "match a key by regex" in {
+      filterKey("group/action/date=2001", ".*date=2001") shouldBe true
+      filterKey("group/action/date=2002", ".*date=2001") shouldBe false
+      filterKey("group/other-action/date=2002", "group/.*/date=2002") shouldBe true
+      filterKey("other-group/other-action/date=2002", "group/.*/date=2002") shouldBe false
+    }
+    "filter by date two levels deep" in {
+      List(
+        "group/some-action/date=2001/object1.json",
+        "group/some-action/date=2002/object2.json",
+        "group/an-action/date=2001/object3.json",
+        "group/an-action/date=2002/object4.json",
+      ).filter(filterKey(_,"group/.*/date=2001/.*")) shouldBe List(
+        "group/some-action/date=2001/object1.json",
+        "group/an-action/date=2001/object3.json"
+      )
     }
   }
 }
